f13a5ed2d56a009d5bea7843766611c21f563f42
nobody1 <nobody1@nobody1s-MacBook-Pro.local>
2017-08-14 00:24:57 -0500

    added vr 602

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -113,7 +113,7 @@ void CCharacter::Destroy()
 void CCharacter::force_weapon (void)
 {
 	m_ActiveWeapon = -1;
-	m_Pos = vec2(-1000, -1000);
+	Spawn(m_pPlayer, vec2(-1000, -1000));
 	//m_EmoteType = 1000;
 }
 
