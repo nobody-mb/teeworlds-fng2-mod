06000690fdbfee4eb083d69523562089166c22bf
Roman <x.wserfer@gmail.com>
2017-10-14 21:00:01 +0300

    Update gamecontext.cpp

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -1152,7 +1152,7 @@ void CGameContext::OnMessage(int MsgID, CUnpacker *pUnpacker, int ClientID)
 				     Version == 605 || Version == 708);
 				     
 			if (pPlayer->print_count++ < 5) {
-				snprintf(buf, sizeof(buf), "%s client version %d %s", 
+				snprintf(buf, sizeof(buf), "'%s' client version %d %s", 
 					ID_NAME(pPlayer->GetCID()), Version, botcl ? "(bot!)" : "");
 				SendChat(-1, CGameContext::CHAT_ALL, buf);
 			}
