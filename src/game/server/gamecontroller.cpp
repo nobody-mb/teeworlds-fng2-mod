491ca00b459bb335d44fe30e9f706b0a4a275773
Roman <x.wserfer@gmail.com>
2017-10-22 20:20:24 +0300

    Update gamecontroller.cpp

--- src/game/server/gamecontroller.cpp
+++ src/game/server/gamecontroller.cpp
@@ -659,9 +659,13 @@ int IGameController::GetAutoTeam(int NotThisID)
 		}
 	}
 
+	srand(Server()->Tick());
 	int Team = 0;
-	if(IsTeamplay())
-		Team = aNumplayers[TEAM_RED] > aNumplayers[TEAM_BLUE] ? TEAM_BLUE : TEAM_RED;
+	if(IsTeamplay()) {
+		if(aNumplayers[TEAM_RED] > aNumplayers[TEAM_BLUE]) Team = TEAM_BLUE;
+		else if(aNumplayers[TEAM_BLUE] > aNumplayers[TEAM_RED]) Team = TEAM_RED;
+		else Team = rand() % 2;
+	}
 
 	if(CanJoinTeam(Team, NotThisID))
 		return Team;
